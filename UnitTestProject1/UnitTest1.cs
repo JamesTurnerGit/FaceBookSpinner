e10abbeaa8162094d47063016f141fba8e497fee
James Turner <mail.jt@gmail.com>
2017-04-29 23:00:34 +0100

    wheel class done

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FaceBookSlotsApp;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WheelTests
+    {
+        Wheel wheel;
+        List<string> testNames = new List<string>();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            testNames.Add("Davey Jones");
+            testNames.Add("Bob Ross");
+            testNames.Add("Jane Dunnicliffe");
+            wheel = new Wheel(testNames);
+        }
+
+        [TestMethod]
+        public void HasVelocity()
+        {
+            var velocity = wheel.Velocity;
+
+            Assert.AreEqual(0, velocity);
+        }
+
+        [TestMethod]
+        public void DoesAccellerate()
+        {
+            wheel.Start();
+            wheel.Tick(0.1f);
+
+            var velocity = wheel.Velocity;
+            Assert.IsTrue(velocity > 0);
+        }
+
+        [TestMethod]
+        public void DoesDecellerate()
+        {
+            wheel.Start();
+            wheel.Tick(1f);
+            var startVelocity = wheel.Velocity;
+
+            wheel.Stop();
+            wheel.Tick(1f);
+            var endVelocity = wheel.Velocity;
+
+            Assert.IsTrue(endVelocity < startVelocity);
+        }
+
+        [TestMethod]
+        public void HasNames()
+        {
+            var wheelNameList = wheel.Names;
+            Assert.IsTrue(wheelNameList == testNames);
+        }
+
+        [TestMethod]
+        public void HasLength()
+        {
+            var spacing = wheel.NameSize;
+            var wheelLength = wheel.Length;
+
+            var properLength =  spacing * testNames.Count;
+
+            Assert.AreEqual(properLength, wheelLength);
+        }
+
+        [TestMethod]
+        public void HasPointer()
+        {
+            var pointerPosition = wheel.Pointer;
+            Assert.AreEqual(pointerPosition, 0);
+        }
+
+        [TestMethod]
+        public void PointerMoves()
+        {
+            var pointerPosition = wheel.Pointer;
+            wheel.Start();
+            wheel.Tick(1f);
+            var newPointerPosition = wheel.Pointer;
+
+            Assert.AreNotEqual(pointerPosition, newPointerPosition);
+        }
+
+        [TestMethod]
+        public void PointerLoops()
+        {
+            var pointerPosition = wheel.Pointer;
+            wheel.NameSize = 5;
+            wheel.SpinSpeed = 5;
+            wheel.Start();
+            wheel.Tick(3f);
+            var newPointerPosition = wheel.Pointer;
+
+            Assert.AreEqual(pointerPosition, newPointerPosition);
+        }
+
+
+        [TestMethod]
+        public void CanReturnName()
+        {
+            var pointerName = wheel.NamePointedAt;
+            var FirstName = testNames[0];
+
+            Assert.AreEqual(FirstName, pointerName);
+        }
+
+        [TestMethod]
+        public void NameChanges()
+        {
+            var pointerName = wheel.NamePointedAt;
+            wheel.NameSize = 5;
+            wheel.SpinSpeed = 5;
+            wheel.Start();
+            wheel.Tick(2f);
+
+            var newPointerName = wheel.NamePointedAt;
+
+            Assert.AreNotEqual(pointerName, newPointerName);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Wheel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceBookSlotsApp
+{
+    public class Wheel
+    {
+        private float startVelocity = 0;
+        private float defaultAccelleration = 5;
+        private float defaultDecelleration = 2;
+        private float defaultNameSize = 50;
+        private float defaultspinSpeed = 100;
+        
+        
+        public float Velocity {  get; private set; }
+        public float TargetSpeed { get; set; }
+        public float DecellerationSpeed { get; set; }
+        public float AccellerationSpeed { get; set; }
+        public float NameSize { get; set; }
+        public float SpinSpeed { get; set; }
+        public float Pointer { get; private set; }
+        
+        public float Length
+        {
+            get
+            {
+                return NameSize * Names.Count;
+            }
+            private set
+            {
+               new InvalidOperationException("this property cannot be directly adjusted, change NameSize or the Names list");
+            }
+        }
+
+        public string NamePointedAt
+        {
+            get
+            {
+                int index = Convert.ToInt32(Pointer / NameSize);
+                return names[index];
+            }
+            set
+            {
+                new InvalidOperationException("this property cannot be directly adjusted (cheater)");
+            }
+        }
+
+        private List<string> names = new List<string>() ;
+        public List<String> Names {
+            get
+            {
+                return names;
+            }
+            private set {
+                names = value;
+            }
+        }
+
+        public Wheel(List<string> names)
+        {
+            Velocity = startVelocity;
+            TargetSpeed = Velocity;
+            AccellerationSpeed = defaultAccelleration;
+            DecellerationSpeed = defaultDecelleration;
+            SpinSpeed = defaultspinSpeed;
+            
+            Names = names;
+            NameSize = defaultNameSize;
+        }
+
+        public void Start()
+        {
+            TargetSpeed = SpinSpeed;
+        }
+
+        public void Stop()
+        {
+            TargetSpeed = 0;
+        }
+
+        public void Tick(float timePassed)
+        {
+            AdjustVelocity(timePassed);
+            AdjustPointer(timePassed);
+        }
+
+        private void AdjustPointer(float timePassed) {      
+            Pointer += Velocity * timePassed;
+            if (Pointer >= Length)
+            {
+                Pointer -= Length;
+            }
+        }
+
+        private void AdjustVelocity(float timePassed)
+        {
+            switch (TargetSpeed > Velocity)
+            {
+                case true:
+                    Velocity += AccellerationSpeed * timePassed;
+                    break;
+                case false:
+                    Velocity -= DecellerationSpeed * timePassed;
+                    break;
+            }
+            if (Velocity > SpinSpeed)
+            {
+                Velocity = SpinSpeed;
+            }
+        }
+
+
+    }
+}
